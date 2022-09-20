04a3910f1d91cc2f116987d66e243a93c32eb363
Shacombrio <102573101+Shacombrio@users.noreply.github.com>
2022-09-20 14:42:02 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+
+
+  
+  articles: Article[];
+  constructor(){
+    
+    this.articles = [
+      new Article ('Angular', 'http://angular.io', 0),
+      new Article ('Fullstack', 'http://fullstack.io', 0),
+      new Article ('Angular Homepage', 'http://angular.io', 0),
+      
+    ];
+    
+  }
+  
+  addArticle(title: HTMLInputElement, link: HTMLInputElement):boolean{
+    this.articles.push(new Article (title.value,link.value,0))
+    console.log(this.articles);
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a:Article,b:Article)=>b.votes-a.votes);
+  }
 }
